aca3e92f7cb93688908f1620118b1dc2ce538da6
Kauan <166316814+DeveloperKauan@users.noreply.github.com>
2026-01-04 16:13:00 -0300

    Initialize Tailwind CSS configuration file

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: 'class', // Seu código usa o 'dark mode' via classe, isso é importante
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
